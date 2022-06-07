0ff7febe9c271d45875bdc1e032569d8cb21ccf5
roman garms <38168462+Romano-Garmez@users.noreply.github.com>
2022-06-06 17:29:39 -0700

    first working version

--- src/main/java/com/romangarms/ScreenOnHandler.java
+++ src/main/java/com/romangarms/ScreenOnHandler.java
@@ -4,12 +4,19 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class ScreenOnHandler implements HttpHandler {
 
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        String response = "<h1>Screen is on</h1>";
+        exchange.sendResponseHeaders(200, response.length());
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+
         System.out.println("Turning Screen On");
         Runtime.getRuntime().exec("vcgencmd display_power 1");
     }
