3c744d45340b4813b98dfb6f18973984add51e99
roman garms <romangarms@gmail.com>
2024-07-26 18:42:59 -0700

    working fr fr

--- src/main/java/com/romangarms/ScreenOffHandler.java
+++ src/main/java/com/romangarms/ScreenOffHandler.java
@@ -18,6 +18,10 @@ public class ScreenOffHandler implements HttpHandler {
         System.out.println("Turning Screen Off");
 
         //run this command to disable the screen
-        Runtime.getRuntime().exec("xrandr --output HDMI-1 --off");
+        Runtime.getRuntime().exec("/home/romangarms/TurnOffDisplay.sh");
+    }
+
+    public static void main(String[] args) throws IOException {
+        Runtime.getRuntime().exec("/home/romangarms/TurnOffDisplay.sh");
     }
 }
--- src/main/java/com/romangarms/ScreenOnHandler.java
+++ src/main/java/com/romangarms/ScreenOnHandler.java
@@ -20,6 +20,10 @@ public class ScreenOnHandler implements HttpHandler {
         System.out.println("Turning Screen On");
 
         //run this command to reenable the screen
-        Runtime.getRuntime().exec("xrandr --output HDMI-1 --auto");
+        Runtime.getRuntime().exec("/home/romangarms/TurnOnDisplay.sh");
+    }
+
+    public static void main(String[] args) throws IOException {
+        Runtime.getRuntime().exec("/home/romangarms/TurnOnDisplay.sh");
     }
 }
